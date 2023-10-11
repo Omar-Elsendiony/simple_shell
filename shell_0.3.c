006ec319603008c6db0c0fa8179f2a4d78cf0c9f
Mohammed Amer <loard.amer.2010@gmail.com>
2023-10-11 19:42:06 +0300

    refinement and testing

--- shell_0.3.c
+++ shell_0.3.c
@@ -11,15 +11,13 @@ int _strcmpUnnecessary(char *s1, char *s2, int count)
 
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0' && count != 0)
-    {
+	{
 		i++;
-        count--;
-    }
+		count--;
+	}
 	diff = s1[i] - s2[i];
 	return (diff);
 }
-
-
 
 int _strcmp(char *s1, char *s2)
 {
@@ -32,129 +30,123 @@ int _strcmp(char *s1, char *s2)
 	return (diff);
 }
 
-
 char *CheckEXEInDir(char *path, char *exe, char *buff, int counter)
 {
-    if (counter == 0)
-        return NULL;
-    int result;
-    struct dirent *entry;
-    char passedDirectory[SIZE];
-    char sentDirectory[SIZE];
-    DIR *dir = opendir(path);
-    if (dir == NULL)
-        return (NULL);
-    char *returnedPath;
-    _strncpy(passedDirectory, path, SIZE);
-    _strncpy(sentDirectory, path, SIZE);
+	if (counter == 0)
+		return NULL;
+	int result;
+	struct dirent *entry;
+	char passedDirectory[SIZE];
+	char sentDirectory[SIZE];
+	DIR *dir = opendir(path);
+	if (dir == NULL)
+		return (NULL);
+	char *returnedPath;
+	_strncpy(passedDirectory, path, SIZE);
+	_strncpy(sentDirectory, path, SIZE);
 
-    _strncat(passedDirectory , "/", SIZE);
-    _strncat(sentDirectory , "/", SIZE);
+	_strncat(passedDirectory, "/", SIZE);
+	_strncat(sentDirectory, "/", SIZE);
 
-    _strncat(path , "/", SIZE);
-    _strncat(path , exe, SIZE);
+	_strncat(path, "/", SIZE);
+	_strncat(path, exe, SIZE);
 
-    result = access(path, F_OK);
-    if (result == -1)
-        while ((entry = readdir(dir)) != NULL)
-        {
-            if (entry->d_type == DT_DIR && _strcmp(entry->d_name, ".") != 0 && _strcmp(entry->d_name, "..") != 0 && _strcmp(entry->d_name, ".git") != 0)
-            {
-                _strncat(sentDirectory ,entry->d_name, SIZE);
-                returnedPath = CheckEXEInDir(sentDirectory, exe, buff, counter - 1);
-                if (returnedPath != NULL)
-                {
-                    return (returnedPath);
-                }
-                _strncpy(sentDirectory, passedDirectory, SIZE);
-            }
-        }
-    else
-    {
-        return (path);
-    }
-    return (NULL);
+	result = access(path, F_OK);
+	if (result == -1)
+		while ((entry = readdir(dir)) != NULL)
+		{
+			if (entry->d_type == DT_DIR && _strcmp(entry->d_name, ".") != 0 && _strcmp(entry->d_name, "..") != 0 && _strcmp(entry->d_name, ".git") != 0)
+			{
+				_strncat(sentDirectory, entry->d_name, SIZE);
+				returnedPath = CheckEXEInDir(sentDirectory, exe, buff, counter - 1);
+				if (returnedPath != NULL)
+				{
+					return (returnedPath);
+				}
+				_strncpy(sentDirectory, passedDirectory, SIZE);
+			}
+		}
+	else
+	{
+		return (path);
+	}
+	return (NULL);
 }
 
 char *readPath(char *pathDir, char *delim, char *exe)
 {
-    char *tokens;
-    char path[SIZE] = {0};
-    char currentDir[SIZE] = {0};
+	char *tokens;
+	char path[SIZE] = {0};
+	char currentDir[SIZE] = {0};
 
-    tokens = strtok(pathDir, delim);
+	tokens = strtok(pathDir, delim);
 
-    getcwd(currentDir, SIZE);
-    _strncat(path, currentDir , SIZE);
-    if (CheckEXEInDir(path, exe, path, 3) != NULL)
-    {
-        printf("%s ***********", path);
-        _strncpy(pathDir, path, SIZE);
-        return (pathDir);
-    }
-    path[0] = 0;
-    while (tokens != NULL) {
-        tokens = strtok(NULL, delim);
-        if (tokens == NULL || !_strcmpUnnecessary(tokens, "/mnt/", 3))
-            continue;
-        _strncat(path, tokens, SIZE);
-        if (CheckEXEInDir(path, exe, path, 3) != NULL)
-        {
-            printf("%s ***********", path);
-            _strncpy(pathDir, path, SIZE);
-            return (pathDir);
-        }
-        path[0] = 0;
-    }
+	getcwd(currentDir, SIZE);
+	_strncat(path, currentDir, SIZE);
+	if (CheckEXEInDir(path, exe, path, 3) != NULL)
+	{
+		_strncpy(pathDir, path, SIZE);
+		return (pathDir);
+	}
+	path[0] = 0;
+	while (tokens != NULL)
+	{
+		tokens = strtok(NULL, delim);
+		if (tokens == NULL || !_strcmpUnnecessary(tokens, "/mnt/", 3))
+			continue;
+		_strncat(path, tokens, SIZE);
+		if (CheckEXEInDir(path, exe, path, 3) != NULL)
+		{
+			_strncpy(pathDir, path, SIZE);
+			return (pathDir);
+		}
+		path[0] = 0;
+	}
 }
-
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(int arc, char **argv, char ** envir)
+int main(int arc, char **argv, char **envir)
 {
-    int i, j, k, childID;
-    char *comparator = "PATH";
-    char *path, *delim = ":", *brokenString;
-    
+	int i, j, k, childID;
+	char *comparator = "PATH";
+	char *path, *delim = ":", *brokenString;
 
-    if (execve("/bin/l", argv, envir) == -1)
-    {
-        int parentID = getpid();
-        /*
-         * get environment path variable to parse it
-        */
-        for (i = 0; envir[i] != NULL; i++)
-        {
-            if (envir[i][0] == 'P')
-            {
-                j = 1;
-                while (envir[i][j] == comparator[j] && envir[i][j] != '\0')
-                {
-                    j++;
-                }
-                if (comparator[j] == '\0')
-                {
-                    path = *(envir + i) + j + 1;
-                    break;
-                }
-            }
-        }
+	if (execve("/bin/l", argv, envir) == -1)
+	{
+		int parentID = getpid();
+		/*
+		 * get environment path variable to parse it
+		 */
+		for (i = 0; envir[i] != NULL; i++)
+		{
+			if (envir[i][0] == 'P')
+			{
+				j = 1;
+				while (envir[i][j] == comparator[j] && envir[i][j] != '\0')
+				{
+					j++;
+				}
+				if (comparator[j] == '\0')
+				{
+					path = *(envir + i) + j + 1;
+					break;
+				}
+			}
+		}
 
-        path = readPath(path, delim, "s");
-        if (path == NULL)
-            perror("could not find path");
-        else
-        {
-            fork();
-            if (getpid() != parentID)
-                if (execve(path, argv, envir) == -1)
-                    perror("Could not execve");
-        }
-    }
-
-
+		path = readPath(path, delim, "/usr/bin/ls");
+		if (path == NULL)
+			perror("could not find path");
+		else
+		{
+			fork();
+			if (getpid() != parentID)
+				if (execve(path, argv, envir) == -1)
+					perror("Could not execve");
+		}
+	}
 }
